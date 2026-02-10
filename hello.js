6013effd4faf84406d87f8949f07c6a1da735a2d
Leonardo <lcat140507@gmail.com>
2026-02-10 11:23:46 +0000

    adicionar script para manipular string

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+var str = "hello";
+var h=str[2];
+var i= str.indexOf("o");
+var s = str.split("l");
+console.log("O indice de o é: " + i);
+console.log("O caracter na posição 2 é: " + h);
+console.log("vai separar em todos os l: " + s);

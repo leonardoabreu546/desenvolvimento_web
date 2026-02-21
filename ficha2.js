82e096b62723d26f9e55230241e73ff1d9848e9f
Leonardo <lcat140507@gmail.com>
2026-02-21 11:56:13 +0000

    criar função calcular imc

---
+++ ficha2.js
@@ -0,0 +1,23 @@
+function calcularIMC(peso, altura) {
+
+    imc = peso / (altura * altura);
+
+    if (imc < 18.5) {
+        return "Abaixo do peso";
+    }
+
+    else if (imc >= 18.5 && imc < 25) {
+        return "Peso normal";
+    }
+
+    else if (imc >= 25 && imc < 30) {
+        return "Acima do peso";
+    }   
+
+    else if (imc >= 30) {
+        return "Obesidade";
+    }
+}
+
+var imc1 = calcularIMC(70, 1.75);
+console.log(imc1);

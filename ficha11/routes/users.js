0ce57d1e148a8037a1fa46a7f6c28dfaa9d6c363
Leonardo <lcat140507@gmail.com>
2026-05-19 09:44:24 +0100

    cria rota get users

--- ficha11/routes/users.js
+++ ficha11/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
+const authMiddleware = require('../middleware/authMiddleware');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authMiddleware, async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
 });
 
 module.exports = router;

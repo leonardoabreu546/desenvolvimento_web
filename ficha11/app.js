0c96c6e96c2a432baf4561c038e5e0d3334c48d8
Leonardo <lcat140507@gmail.com>
2026-05-19 09:01:24 +0100

    sincronizar com a base de daddos

--- ficha11/app.js
+++ ficha11/app.js
@@ -1,41 +1,33 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+// 1. Importar a Base de Dados e o Modelo
+const db = require('./config/db');
+const User = require('./models/user');
+
+// 2. Importar as Rotas (ainda vamos criar a de auth)
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Sincronizar a Base de Dados
+db.sync()
+    .then(() => console.log('Tabelas sincronizadas com sucesso.'))
+    .catch(err => console.log('Erro ao sincronizar: ' + err));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// 3. Definir as Rotas Principais
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 4. Tratamento de Erros Simples (para APIs)
+app.use(function(req, res) {
+    res.status(404).json({ message: "Rota não encontrada" });
 });
 
 module.exports = app;
